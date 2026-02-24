0d4920e293728173bc5f10cbb220b660945d6e86
Collins <collinsabeiku07@gmail.com>
2026-02-24 17:15:16 +0000

    add query_sample.py to implement sample queries

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,23 @@
+from .models import Book, Library, Librarian, Author
+
+def query_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)
+        return books
+    except Author.DoesNotExist:
+        return []
+
+def list_all_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
+
+def librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return Library.librabrian
+    except Library.DoesNotExist:
+        return []
