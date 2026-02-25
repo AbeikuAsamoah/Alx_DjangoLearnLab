783b6f7ddef7da61501e0adc474945ffa9dc1b8b
Collins <collinsabeiku07@gmail.com>
2026-02-25 17:37:33 +0000

    update user authentications

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
         path('books/', views.list_books, name='book_list'),
         path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_books')
         path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-        path('logout/', LogoutView.as_view(), name='logout'),
+        path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+        path('signup/', SignUpView.as_view(), name='signup')
+
 ]
