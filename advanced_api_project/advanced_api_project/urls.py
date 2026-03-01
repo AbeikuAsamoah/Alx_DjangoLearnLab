c41a88ddad3bd1f67625e8240c3ede030735aa20
Collins <collinsabeiku07@gmail.com>
2026-03-01 13:18:42 +0000

    initialize project

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_blog project.
+URL configuration for advanced_api_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,8 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
