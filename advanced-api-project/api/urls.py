5ec0ac3aca5544f3c6fe32bc56e59df2f9271d3b
Collins <collinsabeiku07@gmail.com>
2026-03-03 15:55:35 +0000

    update url patterns in api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
         path('books/', ListView.as_view(), name='book_list'),
         path('books/<int:pk>/', DetailView.as_view(), name='book_detail'),
         path('books/create/', CreateView.as_view(), name='create_book'),
-        path('books/<int:pk>/update', UpdateView.as_view(), name='update_book'),
-        path('books/<int:pk>/delete', DeleteView.as_view(), name='delete_book'),
+        path('books/update/<int:pk>/', UpdateView.as_view(), name='update_book'),
+        path('books/delete/<int:pk>/', DeleteView.as_view(), name='delete_book'),
 ]
