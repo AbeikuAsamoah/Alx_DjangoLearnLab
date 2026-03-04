f61bc4b1258d4bdc3b17ba34af935c540ca5376b
Collins <collinsabeiku07@gmail.com>
2026-03-04 00:32:07 +0000

    set up filtering for book list

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
@@ -11,6 +13,9 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author', 'publication_year']
 
 class DetailView(generics.RetrieveAPIView):
     """Retrieve a single book by ID (GET)"""
