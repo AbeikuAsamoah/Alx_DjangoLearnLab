fc4b4790a2d9698d05ad6bc1a6106245d46d6418
Collins <collinsabeiku07@gmail.com>
2026-03-07 21:35:24 +0000

    Update views for user profile management

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -16,3 +17,7 @@ class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('login')
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'blog/profile.html'
+    login_urls = 'login'
