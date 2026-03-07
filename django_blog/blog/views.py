d48a12af01f74f7b6e93bb0d6604c6a491aa7df8
Collins <collinsabeiku07@gmail.com>
2026-03-07 20:03:59 +0000

    Add views to handle user login, logout and register

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
 # Create your views here.
+
+class UserLginView(LoginView):
+    template_name = 'login.html'
+
+class UserLogoutView(LogoutView):
+    next_page = '/login/'
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
