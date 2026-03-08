9bf0a5e441a39362941392d8ac3dda2ee9887969
Collins <collinsabeiku07@gmail.com>
2026-03-08 16:52:36 +0000

    Modify URL patterns to handle post creation

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegisterView, ProfileView, HomeView, PostListView
+
+from .views import UserLoginView, UserLogoutView, RegisterView, ProfileView, HomeView, create_post, PostListView
 
 urlpatterns = [
         path('', HomeView.as_view(), name='home'),
-        path('login/', LoginView.as_view(template_name='blog/login.html'), name='login'),
-        path('logout/', LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+        path('login/', UserLoginView.as_view(template_name='blog/login.html'), name='login'),
+        path('logout/', UserLogoutView.as_view(template_name='blog/logout.html'), name='logout'),
         path('register/', RegisterView.as_view(), name='register'),
         path('profile/', ProfileView.as_view(), name='profile'),
+        path('create/', create_post, name='create_posts'),
         path('posts/', PostListView.as_view(), name='posts'),
 ]
