07e62e5e821f34a31483768e1ed046778c0c2128
Collins <collinsabeiku07@gmail.com>
2026-03-12 22:43:59 +0000

    Update comment model to properly handle comments

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
 
 
 # Create your models here.
@@ -14,6 +13,10 @@ class Post(models.Model):
             related_name='posts'
             )
 
+    def __str__(self):
+        return self.title
+
+
 class Comment(models.Model):
     post = models.ForeignKey(
             Post,
@@ -23,8 +26,10 @@ class Comment(models.Model):
     author = models.ForeignKey(
             User,
             on_delete=models.CASCADE,
-            related_name='user_comments'
             )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
